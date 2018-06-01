87328f1ae02c96923ce0763e3dc5a7ca058ab40b
Xiangdong Zeng <xdzeng96@gmail.com>
2018-06-01 17:46:00 +0800

    update font names
    They are now named as "FiraMath-[Weight].otf" in order to be consistent with FiraSans, FiraGO, FiraCode, etc.

--- scripts/build.py
+++ scripts/build.py
@@ -8,7 +8,10 @@ weights = ["thin", "light", "regular", "medium", "bold"]
 sfd_suffix = ".sfdir"
 otf_suffix = ".otf"
 
+print("FontForge version: " + fontforge.version())
+
 for i in weights:
     file_name = src_path + family_name_full + "-" + i + sfd_suffix
     font = fontforge.open(file_name)
     font.generate(otf_path + family_name + "-" + i.capitalize() + otf_suffix, flags=("opentype"))
+print("Generating finished!")
