06061ac98b81b587294f934f9a92f0677ad00f92
Xiangdong Zeng <xdzeng96@gmail.com>
2018-10-04 01:24:46 +0800

    script: avoid using `str.join()` in `build.py`

--- scripts/build.py
+++ scripts/build.py
@@ -9,20 +9,13 @@ import platform
 
 import fontforge
 
-# Even on Windows, we should use `/`, otherwise `include()` in AFDKO feature files will break.
-PATH_SEP         = "/"
+# Even on Windows, we should use `/` for paths, otherwise font generation will raise an error.
 PWD              = os.getcwd()
-
-# SFD_PATH         = PATH_SEP.join([PWD, "src"])
-# FEATURE_PATH     = PATH_SEP.join([PWD, "src", "features"])
-# OTF_PATH         = PATH_SEP.join([PWD, "release", "fonts"])
-
-SFD_PATH         = PWD + "/src/"
-FEATURE_PATH     = PWD + "/src/features/"
-OTF_PATH         = PWD + "/docs/assets/"
-
-TEST_PATH        = PATH_SEP.join([PWD, "test"])
-TEX_PATH         = PATH_SEP.join([PWD, "tex"])
+SFD_PATH         = PWD + "/src"
+FEATURE_PATH     = PWD + "/src/features"
+OTF_PATH         = PWD + "/docs/assets"
+TEST_PATH        = PWD + "/test"
+TEX_PATH         = PWD + "/tex"
 FAMILY_NAME      = "FiraMath"
 FAMILY_NAME_FULL = "fira-math"
 TEST_FILE_NAME   = "font-test"
@@ -37,20 +30,13 @@ def generate_fonts():
     for i in WEIGHTS:
         font_name      = FAMILY_NAME + "-" + i.capitalize()
         font_name_full = FAMILY_NAME_FULL + "-" + i
+        sfdir          = SFD_PATH + "/" + font_name_full + ".sfdir"
+        feature_file   = FEATURE_PATH + "/" + font_name_full + ".fea"
+        otf_file       = OTF_PATH + "/" + font_name + ".otf"
 
-        # sfdir          = PATH_SEP.join([SFD_PATH, font_name_full + ".sfdir"])
-        # feature_file   = PATH_SEP.join([FEATURE_PATH, font_name_full + ".fea"])
-        # otf_file       = PATH_SEP.join([OTF_PATH, font_name + ".otf"])
-
-        # font = fontforge.open(sfdir)
-        # font.mergeFeature(feature_file)
-        # font.generate(otf_file, flags=("opentype"))
-        # print(datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')
-        #       + " '" + font_name + "' " + "generated successfully.")
-
-        font = fontforge.open(SFD_PATH + font_name_full + ".sfdir")
-        font.mergeFeature(FEATURE_PATH + font_name_full + ".fea")
-        font.generate(OTF_PATH + font_name + ".otf", flags=("opentype"))
+        font = fontforge.open(sfdir)
+        font.mergeFeature(feature_file)
+        font.generate(otf_file, flags=("opentype"))
         print(datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')
               + " '" + font_name + "' " + "generated successfully.")
 
