9c6bb817c01f3d2a53a511f5e35e6f9a280ce7e9
Xiangdong Zeng <xdzeng96@gmail.com>
2019-01-27 13:04:43 +0800

    scripts: remove MATH variants from sfd (similar to constants)

--- scripts/math_table.py
+++ scripts/math_table.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
 """Modify OpenType MATH table for Fira Math.
 
-See https://docs.microsoft.com/typography/opentype/spec/math#mathconstants-table.
+See https://docs.microsoft.com/typography/opentype/spec/math.
 """
 
 from __future__ import print_function
@@ -39,70 +38,22 @@ MATH_CONSTANTS_DICT = _get_math_constants()
 
 
 def add_math_table(font):
-    _add_math_constants(font)
-    _add_math_glyph_info(font, MATH_TABLE["MathGlyphInfo"])
-    _add_math_variants(font, MATH_TABLE["MathVariants"])
-
-
-def _add_math_constants(font):
+    # Add MATH constants.
     for name, value in MATH_CONSTANTS_DICT.items():
         exec("font.math." + name + " = " + str(value[font.weight]))
+    _add_math_glyph_info(font, MATH_TABLE["MathGlyphInfo"])
+    # Add MATH variants.
+    _add_math_variants(font, "horizontalVariants")
+    _add_math_variants(font, "verticalVariants")
 
 
 def _add_math_glyph_info(font, math_glyph_info_dict):
+    # TODO
     pass
 
 
-def _add_math_variants(font, math_variants_dict):
-    pass
-
-
-
-
-######### TODO #########
-######### To be removed!
-
-def add_variants(font, variants_dict, variant_type):
-    """Add vertical/horizontal variants.
-    """
-    for glyph_name in variants_dict:
-        _check_glyph_name(font, glyph_name)
-        if variant_type == "vertical":
-            font[glyph_name].verticalVariants = variants_dict[glyph_name]
-        elif variant_type == "horizontal":
-            font[glyph_name].horizontalVariants = variants_dict[glyph_name]
-    font.save()
-
-
-def _check_glyph_name(font, glyph_name, fallback_glyph=0x0020):
-    """The glyph with `glyph_name` will be created as `default_glyph` if not exists.
-
-    Defalut value of `fallback_glyph` is U+0020 SPACE.
-    """
-    glyph_unicode = glyph_name_to_unicode(glyph_name)
-    if glyph_name not in font:
-        font.createChar(glyph_unicode, glyph_name)
-        font.selection.select(fallback_glyph)
-        font.copy()
-        font.selection.select(glyph_name)
-        font.paste()
-
-def _main():
-    weight_lists = ["Thin", "UltraLight", "ExtraLight", "Light", "Book", "Regular", "Medium",
-                    "SemiBold", "Bold", "ExtraBold", "Heavy", "Ultra"]
-    # weight_lists = ["Thin", "Regular", "Ultra"]
-    integral_glyphs = {
-        "uni222B": "uni222B uni222B.display",
-        "uni222C": "uni222C uni222C.display",
-        "uni222D": "uni222D uni222D.display",
-        "uni2A0C": "uni2A0C uni2A0C.display",
-        "uni222E": "uni222E uni222E.display",
-        "uni222F": "uni222F uni222F.display",
-        "uni2230": "uni2230 uni2230.display"}
-    font_list = [open_font(weight) for weight in weight_lists]
-    for font in font_list:
-        add_vertical_variants(font, integral_glyphs)
-
-if __name__ == "__main__":
-    add_math_table(0)
-    # _main()
+def _add_math_variants(font, variants_type):
+    for _glyph, _var in MATH_TABLE["MathVariants"][variants_type].items():
+        glyph, var = str(_glyph), str(" ".join(_var))  # Convert unicode to raw string
+        if glyph in font:
+            exec("font[glyph]." + variants_type + " = var")
