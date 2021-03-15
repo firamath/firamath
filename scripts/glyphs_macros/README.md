7438bc7079c7a2aaebe678aec6e768c55318f08b
Xiangdong Zeng <xdzeng96@gmail.com>
2021-03-16 02:22:57 +0800

    add some glyphs macros [ci skip]

---
+++ scripts/glyphs_macros/README.md
@@ -0,0 +1,1 @@
+This folder contains some ad hoc Glyphs macros for Fira Math. They are not intended for further use and I just leave them here for backup.
---
+++ scripts/glyphs_macros/build_smart_components.py
@@ -0,0 +1,20 @@
+suffix_list = [''] + ['.size{:02}'.format(i) for i in range(1, 16)]
+basename1 = 'topparenthesis'
+basename2 = 'bottomparenthesis'
+
+for i, suffix in enumerate(suffix_list):
+	glyph1 = Font.glyphs[basename1 + suffix]
+	for layer in glyph1.layers:
+		print(layer)
+		component = GSComponent('_smart.' + basename1)
+		component.automaticAlignment = True
+		component.smartComponentValues['size'] = i
+		layer.shapes = [component]
+	glyph2 = Font.glyphs[basename2 + suffix]
+	for layer in glyph2.layers:
+		print(layer)
+		component = GSComponent(basename1 + suffix)
+		component.scale = (1, -1)
+		layer.shapes = [component]
+		for shape in layer.shapes:
+			shape.automaticAlignment = True
