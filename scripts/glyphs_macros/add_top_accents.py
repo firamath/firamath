54e51ea8afd9529f6a79be55ba927726401110c0
Xiangdong Zeng <xdzeng96@gmail.com>
2021-04-14 15:04:34 +0800

    update add_top_accents.py [ci skip]

--- scripts/glyphs_macros/add_top_accents.py
+++ scripts/glyphs_macros/add_top_accents.py
@@ -1,12 +1,19 @@
+def getTopAccent(layer):
+	if topAnchor := layer.anchors['top']:
+		return topAnchor.position.x
+	if layer.components:
+		if topAnchor := layer.components[0].componentLayer.anchors['top']:
+			return topAnchor.position.x
+	top = layer.bounds.origin.y + layer.bounds.size.height
+	topNodes = []
+	for path in layer.paths:
+		for node in path.nodes:
+			if node.y == top:
+				topNodes.append(node.x)
+	return sum(topNodes) / len(topNodes)
+
 for l in Font.currentTab.layers:
 	if not isinstance(l, GSControlLayer):
 		print(l.parent.name)
 		for layer in l.parent.layers:
-			top = layer.bounds.origin.y + layer.bounds.size.height
-			topNodes = []
-			for path in layer.paths:
-				for node in path.nodes:
-					if node.y == top:
-						topNodes.append(node.x)
-			topAccent = round(sum(topNodes) / len(topNodes))
-			layer.userData['math'] = {'topAccent': topAccent}
+			layer.userData['math'] = {'topAccent': round(getTopAccent(layer))}
